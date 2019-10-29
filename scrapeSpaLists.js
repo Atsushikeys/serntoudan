f443ff228c7111ec00a69016e39d4bbb2451b376
atsushikeys <atsushi.dans.keyboard@gmail.com>
2019-10-29 23:07:19 +0900

    finish scraping

--- scrapeSpaLists.js
+++ scrapeSpaLists.js
@@ -4,31 +4,33 @@
 //1~84の銭湯名を取得する
 function get84Spas() {
   var spaListSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("銭湯リスト作成用");
-  var arr = [];
   
   for (var i = 1; i < 85; i++) {
     try {
-      spaListSheet.getRange(i + 1, 2).setValue(scrapeOneSpaName(i));
+      spaListSheet.getRange(i + 1, 7).setValue(scrapeOneSpa(i));
     } catch (ex) {
-      spaListSheet.getRange(i + 1, 2).setValue(i + "行目は存在しないページ");
+      spaListSheet.getRange(i + 1, 7).setValue("nonInfo");
     }
   }
-  Logger.log(arr);
 }
 
 
 
-//https://aichi1010.jp/page/detail/l/spaUrrNumber に含まれる銭湯名をスクレイピングする関数
-function scrapeOneSpaName(spaUrlNumber){
+//https://aichi1010.jp/page/detail/l/spaUrrNumber に含まれる銭湯情報をスクレイピングする関数
+function scrapeOneSpa(spaUrlNumber){
   
   //Getリクエストを投げて、戻り値(Webサイト全体のHTML)を文字列に変換し、格納
-  var res = UrlFetchApp.fetch("https://aichi1010.jp/page/detail/l/" + spaUrlNumber).getContentText();
-
-  var startIndex = res.indexOf("<h1>",res.indexOf("<h1>") + 1);
-  var endIndex = res.indexOf("<span>",startIndex);
+  var res = UrlFetchApp.fetch("https://aichi1010.jp/page/detail/l/" + spaUrlNumber ).getContentText();
   
-  var spaName = res.substring(startIndex + 4,endIndex);
 
-  return spaName
+  var searchFromTag = "<dt>住所</dt><dd>";
+  var searchToTag = "</dd>";
+  
+  var startIndex = res.indexOf(searchFromTag);
+  var endIndex = res.indexOf(searchToTag,startIndex);
+  
+  var spaInfo = res.substring(startIndex + searchFromTag.length,endIndex);
+  
+  return spaInfo
   
 }
