d0d8755c1df41ea7599356a43a12f6fee4494c03
atsushikeys <atsushi.dans.keyboard@gmail.com>
2019-10-30 23:42:41 +0900

    URLをforカウンタではなく、スプレッドシート記載のものから取得するように変更

--- scrapeSpaLists.js
+++ scrapeSpaLists.js
@@ -6,10 +6,11 @@ function get84Spas() {
   var spaListSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("銭湯リスト作成用");
   
   for (var i = 1; i < 85; i++) {
+    var spaSheetUrlValue = spaListSheet.getRange(i+1, 2).getValue();
     try {
-      spaListSheet.getRange(i + 1, 7).setValue(scrapeOneSpa(i));
+      spaListSheet.getRange(i + 1, 15).setValue(scrapeOneSpa(spaSheetUrlValue));
     } catch (ex) {
-      spaListSheet.getRange(i + 1, 7).setValue("nonInfo");
+      spaListSheet.getRange(i + 1, 15).setValue("nonInfo");
     }
   }
 }
@@ -17,10 +18,11 @@ function get84Spas() {
 
 
 //https://aichi1010.jp/page/detail/l/spaUrrNumber に含まれる銭湯情報をスクレイピングする関数
-function scrapeOneSpa(spaUrlNumber){
+// 2019/10/30 リファクタ案 => urlをforのカウンタから生成するのではなく、sheetのurlカラムを取ってくるようにすれば、順番が変わっても関係ない。
+function scrapeOneSpa(spaUrl){
   
   //Getリクエストを投げて、戻り値(Webサイト全体のHTML)を文字列に変換し、格納
-  var res = UrlFetchApp.fetch("https://aichi1010.jp/page/detail/l/" + spaUrlNumber ).getContentText();
+  var res = UrlFetchApp.fetch(spaUrl).getContentText();
   
 
   var searchFromTag = "<dt>住所</dt><dd>";
